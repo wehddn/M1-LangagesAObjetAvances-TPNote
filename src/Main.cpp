5c4eca9275846d9d2cc484980d4eec652e96ac4a
GRAFF nicolas <nicolas.graff@etu.u-paris.fr>
2022-10-28 12:17:17 +0200

    Ajout des tests

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,8 +16,8 @@ int main(){
     
     Arete a1{s1, s2, 1};
     Arete a11{s1, s2, 1};
-    Arete a2{s3,s4,3};
-    Arete a3{"s3", "s4", 2};
+    Arete a2{"s3","s4",3};
+    Arete a3{s3, s4, 2};
     Arete a4{s1, s5, 2};
     Arete a5{s1, s3, 5};
     Arete a1copy{a1};
@@ -36,10 +36,13 @@ int main(){
     cout << "\n";
 
     cout << "Test aretes : \n";
-    cout << "test << de a1 : " << a1 <<"\n";
+    cout << "test << de a1, créée à partir de deux sommets et d'un poids : " << a1 <<"\n";
+    cout << "test << de a2, créée à partir de deux étiquettes et d'un poids : " << a2 <<"\n";
     cout << "test << pour une copy de a1 : " << a1copy <<"\n";
     cout << "test getS1 : " << a1.getS1()->getEtiquette() <<"\n";
     cout << "test getPoids : " << a1.getPoids() << "\n"; 
+    cout << "test des arêtes avec les mêmes sommets : " << "\n";
+    cout << "avant de modifier le poids du sommet s1 : " << a1 << "; " <<  a4 << "\n";
     cout << "\n";
 
     cout << "Test graphe : \n";
@@ -56,17 +59,11 @@ int main(){
     cout << "test ajoute_arete à partir de deux sommets existants et d'un poids: " << g1 <<"\n";
     g1.ajoute_arete("s1", "s5", 3);
     cout << "test ajoute_arete à partir de deux étiquettes et d'un poids " << g1 <<"\n";
-    g1.symetrise();
-    cout << "test symetrise : " << g1 <<"\n";
-    Graphe g2 = g1.kruskal();
-    cout << "test kruskal : " << g2 <<"\n";
-    GarbageCollector::test();
-
-    cout << "\ntest g1copy : " << g1copy <<"\n";
-    g1copy.ajoute_arete(s3, s5, 1);
-    cout << "test ajoute_arete à partir de deux sommets existants et d'un poids: " << g1copy <<"\n";
-    g1copy.ajoute_arete("s1", "s5", 3);
-    cout << "test g1copy : " << g1copy <<"\n";
+    cout << "\ng1qcopy : " << g1copy << "\n";
+    g1copy.symetrise();
+    cout << "test symetrise : " << g1 << "\n";
+    Graphe g2 = g1copy.kruskal();
+    cout << "test kruskal : " << g2 << "\n";
 
     return 0;
 }
